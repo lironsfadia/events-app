a441b048e07ebd70bfd255ab6ae9ebd14525a92d
Liron <liron.ssss@gmail.com>
2024-09-15 17:05:29 +0300

    updated fetch attenddace fetching.

--- app/(event)/[id].tsx
+++ app/(event)/[id].tsx
@@ -4,13 +4,31 @@ import { Stack, useLocalSearchParams } from 'expo-router';
 import { View, Text, Image, Pressable, ActivityIndicator } from 'react-native';
 import { formatDate } from '@/utils/date';
 import { supabase } from '@/utils/supabase';
+import { useAuth } from '@/contexts/AuthProvider';
 
 function EventScreen() {
   const { id } = useLocalSearchParams();
+  const { user } = useAuth();
+  const [attendance, setAttendance] = useState<any | null>(null);
   const [event, setEvent] = useState<any | null>(null);
   const [error, setError] = useState<unknown | null>(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
+    const fetchAttendance = async () => {
+      try {
+        setLoading(true);
+        let { data, error } = await supabase
+          .from('attendance')
+          .select('*')
+          .eq('event_id', id)
+          .eq('user_id', user?.id)
+          .single();
+        setAttendance(data);
+        setLoading(false);
+      } catch (e: unknown) {
+        setError(e);
+      }
+    };
     const fetchEvent = async () => {
       try {
         setLoading(true);
@@ -21,7 +39,7 @@ function EventScreen() {
         setError(e);
       }
     };
-
+    fetchAttendance();
     fetchEvent();
   }, [id]);
 
@@ -39,6 +57,21 @@ function EventScreen() {
   const { title, image_uri, date, desc } = event;
   const time = formatDate(date);
 
+  const onAttend = async () => {
+    try {
+      await supabase
+        .from('attendance')
+        .insert([{ event_id: id, user_id: user?.id }])
+        .select()
+        .single();
+
+      setAttendance({ event_id: id, user_id: user?.id });
+      alert('You have successfully RSVPed to this event');
+    } catch (e: unknown) {
+      alert('An error occurred while RSVPing to this event');
+    }
+  };
+
   return (
     <View className="flex-1 bg-white p-3">
       <Stack.Screen
@@ -53,9 +86,13 @@ function EventScreen() {
 
       <View className="border-grey-100 absolute bottom-1 left-0 right-0 flex-1 flex-row items-center justify-between border-t-2 bg-white p-5">
         <Text className="p-3 text-xl font-semibold">Free</Text>
-        <Pressable className="rounded-md bg-red-400 p-5">
-          <Text className="font-bold text-white">Join and RSVP</Text>
-        </Pressable>
+        {attendance ? (
+          <Text className="font-bold text-green-600">You Are Attending!</Text>
+        ) : (
+          <Pressable onPress={onAttend} className="rounded-md bg-red-400 p-5">
+            <Text className="font-bold text-white">Join and RSVP</Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
